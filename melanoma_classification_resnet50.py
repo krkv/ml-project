f4f0906419bfde4f768bea809dafe30127db12c1
Rodion Kryuchkov <rodion.krjutskov@gmail.com>
2020-12-14 21:55:28 +0200

    clean up

--- melanoma_classification_resnet50.py
+++ melanoma_classification_resnet50.py
@@ -2,42 +2,26 @@ import pandas as pd
 import numpy as np
 import tensorflow as tensorflow
 
-from keras import models, regularizers, layers, optimizers, losses, metrics
+from keras import models, regularizers, layers, optimizers
 from keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.applications.resnet50 import ResNet50
-
-import json
 
 train2020 = pd.read_csv(
     "../input/siim-isic-melanoma-classification/train.csv", dtype=str)
 test2020 = pd.read_csv(
     "../input/siim-isic-melanoma-classification/test.csv", dtype=str)
-train2019 = pd.read_csv(
-    "../input/isic-2019/ISIC_2019_Training_GroundTruth.csv", dtype=str)
+
+neg2020 = train2020[train2020['target'] == '0']
+pos2020 = train2020[train2020['target'] == '1']
+
 bal2020 = pd.concat([neg2020[:2000], pos2020])
-
-
-def append_jpg(fn):
-    return fn+".jpg"
 
 
 def append_png(fn):
     return fn+".png"
 
 
-def add_target(mel):
-    if(mel == "1.0"):
-        return "1"
-    else:
-        return "0"
-
-
-train2019["file_name"] = train2019["image"].apply(append_jpg)
-train2019["target"] = train2019["MEL"].apply(add_target)
-
-train2020["file_name"] = train2020["image_name"].apply(append_png)
 test2020["file_name"] = test2020["image_name"].apply(append_png)
-
 bal2020["file_name"] = bal2020["image_name"].apply(append_png)
 
 train_datagen = ImageDataGenerator(
@@ -101,16 +85,11 @@ model.add(layers.Dense(1, activation='sigmoid'))
 
 model.compile(optimizer=optimizers.Adam(learning_rate=0.0001),
               loss='binary_crossentropy', metrics=['AUC'])
-
-print(model.summary())
-
 model.fit_generator(train_generator, epochs=5)
 
 predictions = model.predict_generator(test_generator)
 
 submission = pd.DataFrame()
-
 submission['image_name'] = test2020['image_name']
 submission['target'] = predictions
-
 submission.to_csv('submission.csv', index=False)
